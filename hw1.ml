f9abd4990f6044df58f1afbb65c82c7f0a174b1d
David S. Li <ds.li@outlook.com>
2018-09-16 22:37:32 -0400

    Applying fixes after testing parts 1, 3, 7

--- hw1.ml
+++ hw1.ml
@@ -1,9 +1,16 @@
+(*David S. Li - 110328771
+CSE 307 HW 1*)
+
+(*declaring NoneSuch exception for parts 6 and 7*)
+exception NoneSuch
+
 (*Part 1 - indivisible*)
 let rec indivisible i = function
 | [] -> true
-| x::xs -> ((x mod i != 0) && (indivisible i xs))
+| x::xs -> if (i mod x == 0) then false else (indivisible i xs)
 
 (*Part 2 - prime*)
+
 
 (*Part 3 - inorder*)
 let rec inorder = function
@@ -16,9 +23,13 @@ let rec swapfirst = function
 | x1 :: x2 :: xs -> if (x1 > x2) then (x2 :: x1 :: xs) else swapfirst (x2 :: xs)
 
 (*Part 5 - poorsort*)
+let rec poorsort_rec i = function
+| [] -> ([], i)
+| x1 :: x2 :: xs -> if (x1 > x2) then poorsort_rec(x2 :: x1 :: xs, i + 1) else poorsort_rec(x2 :: xs, i)
+
 let rec poorsort = function
 | [] -> ([], 0)
-(*| x1 :: x2 :: xs -> if inorder(x1::x2::xs) then ((x1::x2::xs),0) else if (x1 > x2)*)
+| xs -> if inorder(xs) then ((xs),0) else poorsort_rec((xs, 0))
 
 (*Part 6 - first_repeat*)
 let rec contains_elem n= function
@@ -27,3 +38,14 @@ let rec contains_elem n= function
 
 let rec first_repeat = function
 | [] -> raise NoneSuch
+| x1 :: xs -> if contains_elem(x1 xs) then x1 else first_repeat xs
+
+(*Part 7 - supdivisor*)
+let rec factorial =
+  if (x <= 0) then 1 else x * factorial(x - 1);;
+
+let rec supdivisor_rec i =
+ if (x mod 2**i == 0) then supdivisor_rec(x, i + 1) else (i - 1)
+
+let rec supdivisor =
+if (x < 0) then raise NoneSuch else supdivisor_rec(factorial(x), 0)

396e4930865488f1eca271a666ba9558d1f09164
David S. Li <ds.li@outlook.com>
2018-09-17 19:28:42 -0400

    Modifications to primes, poorsort, supdivisor

--- hw1.ml
+++ hw1.ml
@@ -10,7 +10,18 @@ let rec indivisible i = function
 | x::xs -> if (i mod x == 0) then false else (indivisible i xs)
 
 (*Part 2 - prime*)
+(*generates a list of numbers between 2 and the number originally specified*)
+let rec generate_list top_bound = function (*x, implicit arg, should start at 2*)
+| x -> if (x > top_bound) then [] else (x :: (generate_list top_bound (x + 1)))
 
+(*finds prime numbers less than the number originally specified*)
+(*params: x - originally specified number, prime_nos - list of prime numbers, nos - see below*)
+let rec primes_rec x prime_nos = function (*nos - short for numbers between 2 and x (inclusive) is implicit*)
+| [] -> prime_nos
+| n :: nos -> if (indivisible n (generate_list (n - 1) 2)) then (primes_rec x (n::prime_nos) nos) else (primes_rec x prime_nos nos)
+
+let primes = function
+| x -> (primes_rec x [] (generate_list x 2));;
 
 (*Part 3 - inorder*)
 let rec inorder = function
@@ -25,11 +36,11 @@ let rec swapfirst = function
 (*Part 5 - poorsort*)
 let rec poorsort_rec i = function
 | [] -> ([], i)
-| x1 :: x2 :: xs -> if (x1 > x2) then poorsort_rec(x2 :: x1 :: xs, i + 1) else poorsort_rec(x2 :: xs, i)
+| x1 :: x2 :: xs -> if (x1 > x2) then (poorsort_rec x2 :: x1 :: xs (i + 1)) else (poorsort_rec x2 :: xs i)
 
 let rec poorsort = function
 | [] -> ([], 0)
-| xs -> if inorder(xs) then ((xs),0) else poorsort_rec((xs, 0))
+| xs -> if (inorder xs) then ((xs),0) else (poorsort_rec xs 0)
 
 (*Part 6 - first_repeat*)
 let rec contains_elem n= function
@@ -41,11 +52,11 @@ let rec first_repeat = function
 | x1 :: xs -> if contains_elem(x1 xs) then x1 else first_repeat xs
 
 (*Part 7 - supdivisor*)
-let rec factorial =
+let rec factorial x =
   if (x <= 0) then 1 else x * factorial(x - 1);;
 
-let rec supdivisor_rec i =
- if (x mod 2**i == 0) then supdivisor_rec(x, i + 1) else (i - 1)
+let rec supdivisor_rec x i =
+ if (x mod (int_of_float (2.0 **(float_of_int i))) == 0) then (supdivisor_rec x (i + 1)) else (i - 1)
 
-let rec supdivisor =
-if (x < 0) then raise NoneSuch else supdivisor_rec(factorial(x), 0)
+let rec supdivisor x =
+if (x < 0) then raise NoneSuch else (supdivisor_rec (factorial x) 0)

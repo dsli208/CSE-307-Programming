c08f2d1812e7636501f34063ed6b7596ea75f1cd
David S. Li <ds.li@outlook.com>
2018-09-18 21:44:44 -0400

    Fixing bug in poorsort

--- OCAML/Test Files/poorsort.ml
+++ OCAML/Test Files/poorsort.ml
@@ -12,9 +12,12 @@ let rec poorsort_rec i = function
 | x :: [] -> (x :: [], i)
 | x1 :: x2 :: xs -> let (a,b) = (poorsort_rec i (x2::xs)) in if (x1 > x2) then (poorsort_rec (i + 1) (x2 :: x1 :: xs)) else ((x1::a), b)
 
+let rec poorsort_done = function
+| (a, b) -> if inorder a then (a, b) else poorsort_done (poorsort_rec b a)
+
 let rec poorsort = function
 | [] -> ([], 0)
-| xs -> if (inorder xs) then ((xs),0) else (poorsort_rec 0 xs)
+| xs -> if (inorder xs) then ((xs),0) else poorsort_done (poorsort_rec 0 xs)
 
 let (list, i_ret) = poorsort [3;2;4];;
 print_list list;;
